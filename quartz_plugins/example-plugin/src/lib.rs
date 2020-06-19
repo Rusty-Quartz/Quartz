58568557e15eb8682ff8f05c4409c091b9a62a42
maddymakesgames <madelinemakesgames@gmail.com>
2020-06-18 22:22:29 -0400

    add example plugin

---
+++ quartz_plugins/example-plugin/src/lib.rs
@@ -0,0 +1,40 @@
+use quartz_plugin_lib::{Listeners, get_quartz_info, ClientBoundPacket, PROTOCOL_VERSION};
+use serde_json::json;
+
+#[no_mangle]
+pub fn get_plugin_info() -> quartz_plugin_lib::PluginInfo {
+    quartz_plugin_lib::PluginInfo {
+        name: "Test plugin".to_owned(),
+        version: "1.0.0".to_owned(),
+        listeners: vec![Listeners::StatusResponse],
+        quartz_data: get_quartz_info()
+    }
+}
+
+
+#[no_mangle]
+pub fn on_status_response(packet: &mut ClientBoundPacket) -> ClientBoundPacket {
+    // packet should always be StatusResponsePacket but checking never hurts
+    match packet {
+        ClientBoundPacket::StatusResponse {..} => {
+            println!("Plugin is running :coolchamp:");
+            ClientBoundPacket::StatusResponse {
+                json_response: json!({
+                    "version": {
+                        "name": "1.17 lel",
+                        "protocol": PROTOCOL_VERSION
+                    },
+                    "players": {
+                        "max": 9001,
+                        "online": 0,
+                        "sample": [] // Maybe implement this in the future
+                    },
+                    "description": "Server has been hijacked by a plugin"
+                }).to_string()
+            }
+        }
+        _ => {
+            panic!("Something has gone horribly wrong");
+        }
+    }
+}
