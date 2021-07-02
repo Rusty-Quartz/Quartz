ce2dbaeb28efd2f48f682141f12b30b6e41c3d09
Ian Smith <isilversmith810@gmail.com>
2021-07-02 14:42:05 -0400

    Add write functionality for CompactStateBuffer

--- quartz/src/world/chunk/encoder.rs
+++ quartz/src/world/chunk/encoder.rs
@@ -14,21 +14,72 @@ impl CompactStateBuffer<Vec<i64>> {
     }
 }
 
-impl<'a, T: AsRef<[i64]> + ?Sized> CompactStateBuffer<&'a T> {
+impl<T> CompactStateBuffer<T> {
+    fn advance_cursor(cursor: &mut usize, bit_index: usize, bits: usize) {
+        if bit_index + 2 * bits > 64 {
+            *cursor += 64 - bit_index;
+        } else {
+            *cursor += bits;
+        }
+    }
+
+    fn write_index_to(dest: &mut [i64], cursor: &mut usize, index: usize, bits: usize) -> bool {
+        let long_index = *cursor / 64;
+        let bit_index = *cursor % 64;
+
+        debug_assert!(index < (1 << bits), "Index must be able to fit in allocated bits");
+
+        let insertion = (index as i64) << bit_index;
+        match dest.get_mut(long_index) {
+            Some(long) => *long |= insertion,
+            None => return false
+        }
+
+        Self::advance_cursor(cursor, bit_index, bits);
+        true
+    }
+
+    fn write_index_to_vec(dest: &mut Vec<i64>, cursor: &mut usize, index: usize, bits: usize) {
+        if *cursor / 64 >= dest.len() {
+            dest.push(0);
+        }
+        debug_assert!(
+            Self::write_index_to(&mut *dest, cursor, index, bits),
+            "Failed to expand vec to required capacity in compact state buffer."
+        );
+    }
+}
+
+impl<T: AsRef<[i64]>> CompactStateBuffer<T> {
     pub fn read_index(&mut self, bits: usize) -> Option<usize> {
         let slice = self.inner.as_ref();
         let long_index = self.cursor / 64;
         let bit_index = self.cursor % 64;
 
-        let start = (*slice.get(long_index)?) >> bit_index & ((1 << bits) - 1);
-        let end = if bit_index + bits > 64 {
-            (*slice.get(long_index + 1)? & ((1 << bit_index + bits - 64) - 1)) << 64 - bit_index
-        } else {
-            0
-        };
+        let index = (*slice.get(long_index)? as u64 >> bit_index) & ((1u64 << bits) - 1);
 
-        self.cursor += bits;
-        Some((start | end) as usize)
+        Self::advance_cursor(&mut self.cursor, bit_index, bits);
+        Some(index as usize)
+    }
+}
+
+impl CompactStateBuffer<&mut [i64]> {
+    pub fn write_index(&mut self, index: usize, bits: usize) -> bool {
+        Self::write_index_to(self.inner, &mut self.cursor, index, bits)
+    }
+}
+
+impl CompactStateBuffer<&mut Vec<i64>> {
+    pub fn write_index(&mut self, index: usize, bits: usize) -> bool {
+        Self::write_index_to_vec(&mut self.inner, &mut self.cursor, index, bits);
+        true
+    }
+}
+
+impl CompactStateBuffer<Vec<i64>> {
+    pub fn write_index(&mut self, index: usize, bits: usize) -> bool {
+        Self::write_index_to_vec(&mut self.inner, &mut self.cursor, index, bits);
+        true
     }
 }
 
