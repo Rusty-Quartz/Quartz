7145ab3f9ddce3fbe01cbf3d9725e932bb7eac28
maddymakesgames <madelinemakesgames@gmail.com>
2022-07-24 19:58:44 -0400

    Impl Marsaglia polar gaussian random

---
+++ quartz/src/world/chunk/gen/random/marsaglia_polar.rs
@@ -0,0 +1,48 @@
+use std::{cell::Cell, rc::Rc};
+
+use crate::world::chunk::gen::random::RandomSource;
+
+pub struct MarsagliaPolarGaussian {
+    next_next_gaussian: f64,
+    have_next_next_gaussian: bool,
+}
+
+impl MarsagliaPolarGaussian {
+    pub fn new() -> Self {
+        MarsagliaPolarGaussian {
+            next_next_gaussian: 0.0,
+            have_next_next_gaussian: false,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.have_next_next_gaussian = false;
+    }
+
+    // we use a RandomSource param instead of a field
+    // because we can't have circular refs in Rust
+    pub fn next_gaussian<T: RandomSource>(&mut self, random_source: &mut T) -> f64 {
+        if self.have_next_next_gaussian {
+            self.have_next_next_gaussian = false;
+            self.next_next_gaussian
+        } else {
+            let mut d;
+            let mut e;
+            let mut f;
+            loop {
+                d = 2.0 * random_source.next_double() - 1.0;
+                e = 2.0 * random_source.next_double() - 1.0;
+                f = f64::powi(d, 2) + f64::powi(e, 2);
+
+                if f < 1.0 && f != 0.0 {
+                    break;
+                }
+            }
+
+            let g = (-2.0 * f64::ln(f) / f).sqrt();
+            self.next_next_gaussian = e * g;
+            self.have_next_next_gaussian = true;
+            d * g
+        }
+    }
+}
