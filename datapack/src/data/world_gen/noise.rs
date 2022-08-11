6a3784f84d1a870479d36346a089c55a5b71309f
maddymakesgames <madelinemakesgames@gmail.com>
2022-08-11 00:54:36 -0400

    use the proper casing when ser/de on Noise

--- datapack/src/data/world_gen/noise.rs
+++ datapack/src/data/world_gen/noise.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Noise {
+    #[serde(rename = "firstOctave")]
     pub first_octave: i32,
     pub amplitudes: Vec<f64>,
 }
